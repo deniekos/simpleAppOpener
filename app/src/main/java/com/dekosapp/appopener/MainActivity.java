feb75b0658256a87edeb2be964923c91e1e4080c
dekosantoso <deni@engineer.com>
2020-12-16 14:38:24 +0700

    code commit

---
+++ app/src/main/java/com/dekosapp/appopener/MainActivity.java
@@ -0,0 +1,45 @@
+package com.dekosapp.appopener;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView txtPackage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtPackage = (EditText)findViewById(R.id.editTextPackage);
+    }
+
+    public void onClickStartNewActivity(View v) {
+        startNewActivity(this);
+    }
+
+    public void startNewActivity(Context context) {
+        String packageName = txtPackage.getText().toString();
+        Intent intent = context.getPackageManager().getLaunchIntentForPackage(packageName);
+        if (intent != null) {
+            // The apps that correspond with package name inputted is found in your device, so it opened
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(intent);
+        } else {
+            // The apps that correspond with package name inputted is not found in your device, so it opened
+            intent = new Intent(Intent.ACTION_VIEW);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.setData(Uri.parse("market://details?id=" + packageName));
+            context.startActivity(intent);
+        }
+    }
+
+
+}
